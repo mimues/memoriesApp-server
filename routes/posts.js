5c0f9c84af9ad31a8512a416258681ab2535b679
Miguel Muñoz <mmestelles@gmail.com>
2022-07-07 13:50:04 +0200

    make it better

---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { getPost, getPosts, commentPost, getPostsBySearch, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+import auth from '../middlewares/auth.js'
+
+const router = express.Router()
+
+router.get('/search', getPostsBySearch)
+router.get('/', getPosts)
+router.get('/:id', getPost)
+router.post('/', auth, createPost)
+router.patch('/:id', auth, updatePost)
+router.delete('/:id', auth, deletePost)
+router.patch('/:id/likePost', auth, likePost)
+router.post('/:id/commentPost', commentPost)
+
+export default router
